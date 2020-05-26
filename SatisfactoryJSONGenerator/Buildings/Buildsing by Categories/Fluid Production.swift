38983228ee5e43c94d29fdd5c50d3dc197557496
Vyacheslav Nagornyak <vnagornyak@x1group.com>
2020-05-26 17:57:30 +0300

    Fixed typos in names for JSON generation

--- SatisfactoryJSONGenerator/Buildings/Buildsing by Categories/Fluid Production.swift
+++ SatisfactoryJSONGenerator/Buildings/Buildsing by Categories/Fluid Production.swift
@@ -1,5 +1,5 @@
-let waterExctractor = Building(name: "Water Exctracor", buildingType: .fluidProduction)
-let oilExctractor = Building(name: "Oil Exctractor", buildingType: .fluidProduction)
+let waterExctractor = Building(name: "Water Extractor", buildingType: .fluidProduction)
+let oilExctractor = Building(name: "Oil Extractor", buildingType: .fluidProduction)
 let refinery = Building(name: "Refinery", buildingType: .fluidProduction)
 
 let FluidProduction = [
--- SatisfactoryJSONGenerator/Buildings/Buildsing by Categories/Pipelines.swift
+++ SatisfactoryJSONGenerator/Buildings/Buildsing by Categories/Pipelines.swift
@@ -1,5 +1,5 @@
 let pipeline = Building(name: "Pipeline", buildingType: .pipelines)
-let pipelineJunkctionCross = Building(name: "Pipeline Junktion Cross", buildingType: .pipelines)
+let pipelineJunkctionCross = Building(name: "Pipeline Junction Cross", buildingType: .pipelines)
 let pipelinePump = Building(name: "Pipeline Pump", buildingType: .pipelines)
 
 let Pipelines = [

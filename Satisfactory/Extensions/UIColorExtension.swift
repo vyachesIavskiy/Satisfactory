bcf640ba4c4c055b0df325244e69777b311a3e98
Vyacheslav Nagornyak <vnagornyak@x1group.com>
2020-05-26 17:59:13 +0300

    Added factory basic colors

---
+++ Satisfactory/Extensions/UIColorExtension.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+extension UIColor {
+    static let factoryPrimaryColor = UIColor(displayP3Red: 243 / 255, green: 77 / 255, blue: 17 / 255, alpha: 1)
+    static let factorySecondaryColor = UIColor(displayP3Red: 29 / 255, green: 34 / 255, blue: 67 / 255, alpha: 1)
+}

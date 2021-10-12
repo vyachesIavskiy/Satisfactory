b1016aca8697ac0f0d77120fc4595afbd2dd4374
Slava <nagornyak.slava@gmail.com>
2021-10-12 15:15:54 +0300

    Removed redundant description

--- SatisfactoryJSONGenerator/Tier.swift
+++ SatisfactoryJSONGenerator/Tier.swift
@@ -9,19 +9,3 @@ enum Tier: Int, Encodable {
     case tier7 = 7
     case tier8 = 8
 }
-
-extension Tier: CustomStringConvertible {
-    var description: String {
-        switch self {
-        case .tier0: return "Building HUB"
-        case .tier1: return "Tier 1"
-        case .tier2: return "Tier 2"
-        case .tier3: return "Tier 3"
-        case .tier4: return "Tier 4"
-        case .tier5: return "Tier 5"
-        case .tier6: return "Tier 6"
-        case .tier7: return "Tier 7"
-        case .tier8: return "Tier 8"
-        }
-    }
-}

2c8985b4bb1ec7ad2999da30bcb3b5ac04315bb9
SlavaBreath <nagornyak.slava@gmail.com>
2023-05-14 19:59:18 +0200

    Added v1.4 disclaimer with what's new in this update

--- Shared/UI/ContentView.swift
+++ Shared/UI/ContentView.swift
@@ -1,8 +1,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @AppStorage("disclaimer.shown.v1_4")
+    private var disclaimerForV1_4Shown = false
+    
+    @State private var showDisclaimer = false
+    
     var body: some View {
         ItemListView()
+            .task { @MainActor in
+                guard !disclaimerForV1_4Shown else { return }
+                
+                try? await Task.sleep(nanoseconds: 50)
+                showDisclaimer = true
+            }
+            .fullScreenCover(isPresented: $showDisclaimer) {
+                DisclaimerView()
+            }
+            .onChange(of: showDisclaimer) { newValue in
+                if !showDisclaimer {
+                    disclaimerForV1_4Shown = true
+                }
+            }
     }
 }
 

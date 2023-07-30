b9ec2f17ae17ee6f0ca859ea5825c16956f3ec74
Vyacheslavskiy <nagornyak.slava@gmail.com>
2023-07-30 11:59:16 +0200

    Added more convenience anchors to CGRect

--- Shared/Extensions/CGRect+Points.swift
+++ Shared/Extensions/CGRect+Points.swift
@@ -5,4 +5,8 @@ extension CGRect {
     var topRight: CGPoint { CGPoint(x: maxX, y: minY) }
     var bottomLeft: CGPoint { CGPoint(x: minX, y: maxY) }
     var bottomRight: CGPoint { CGPoint(x: maxX, y: maxY) }
+    var topCenter: CGPoint { CGPoint(x: midX, y: minY) }
+    var bottomCenter: CGPoint { CGPoint(x: midX, y: maxY) }
+    var centerLeft: CGPoint { CGPoint(x: minX, y: midY) }
+    var centerRight: CGPoint { CGPoint(x: maxX, y: midY) }
 }

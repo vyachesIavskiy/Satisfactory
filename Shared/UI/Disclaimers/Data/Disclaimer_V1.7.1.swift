409e1129ccbfa9bd8630c7343d95ee1db6839d62
Vyacheslavskiy <nagornyak.slava@gmail.com>
2023-10-02 09:13:41 +0200

    Added missing app store connect screenshot for iPad 12.9 (2nd gen)

---
+++ Shared/UI/Disclaimers/Data/Disclaimer_V1.7.1.swift
@@ -0,0 +1,27 @@
+
+extension Disclaimer {
+    static var v1_7: Disclaimer {
+        Disclaimer(
+            version: .v1_7,
+            updateMessage: """
+            Ahoj!
+            
+            Another small update that does not break anything 🤞. Although there are some fixes in UI. Hope you enjoy! In any case you can let us know via a feedback in settings!
+            """,
+            changes: [
+                Change(
+                    log: "Saved productions are no longer visible in recipe selection screen for production.",
+                    changeType: .fix
+                ),
+                Change(
+                    log: "Items list has a new UI (again).",
+                    changeType: .addition
+                ),
+                Change(
+                    log: "Production screen is now a separate full screen view. We hope this way it will not so confusing.",
+                    changeType: .addition
+                )
+            ]
+        )
+    }
+}

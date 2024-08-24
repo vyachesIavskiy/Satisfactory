85057d172896aef60bfb4e74cf590b31d5a3308d
Slava Nagornyak <nagornyak.slava@gmail.com>
2024-08-24 17:46:04 +0200

    Removed redundant information from AppInfo screen

--- Shared/Settings/AppInfoView.swift
+++ Shared/Settings/AppInfoView.swift
@@ -2,36 +2,15 @@ import SwiftUI
 import SHStorage
 
 struct AppInfoView: View {
-    @Dependency(\.storageService)
-    private var storageService
-    
     var body: some View {
         List {
-            Section("App") {
-                cell(title: "App version:", subtitle: Bundle.main.appVersion)
-                
-                cell(title: "App build number:", subtitle: Bundle.main.appBuildNumber)
-                
-                cell(
-                    title: "Static configuration version",
-                    subtitle: storageService.staticConfiguration().version.formatted(.number)
-                )
-                
-                cell(
-                    title: "Persistent configuration version",
-                    subtitle: storageService.persistentConfiguration().version.formatted(.number)
-                )
-            }
+            cell(title: "App version:", subtitle: Bundle.main.appVersion)
             
-            Section("Game") {
-                cell(title: "Game update", subtitle: "Update 8")
-                
-                cell(title: "Game branch", subtitle: "Early access")
-                
-                cell(title: "Game version", subtitle: "0.8.3.3")
-                
-                cell(title: "Game build number", subtitle: "273254")
-            }
+            cell(title: "App build number:", subtitle: Bundle.main.appBuildNumber)
+            
+            cell(title: "Game update", subtitle: "Update 8")
+            
+            cell(title: "Game version", subtitle: "0.8.3.3")
         }
         .navigationTitle("App Info")
     }

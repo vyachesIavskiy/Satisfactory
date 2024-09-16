e43307cb435e4ed2acf9dcfd061b5f6da4317b0a
Slava Nagornyak <nagornyak.slava@gmail.com>
2024-09-16 22:54:52 +0200

    Updated building icons Removed somersloop and mercer sphere (bad icons) Added missing generator buildings

--- Packages/SHStorage/Sources/SHGenerator/V2/Parts/Non productionable/Special Parts V2.swift
+++ Packages/SHStorage/Sources/SHGenerator/V2/Parts/Non productionable/Special Parts V2.swift
@@ -10,13 +10,13 @@ private extension Part.Static {
 extension V2.Parts {
     static let hardDrive = Part.Static(id: "part-hard-drive")
     static let coupon = Part.Static(id: "part-ficsit-coupon")
-    static let mercerSphere = Part.Static(id: "part-mercer-sphere")
-    static let somersloop = Part.Static(id: "part-somersloop")
+//    static let mercerSphere = Part.Static(id: "part-mercer-sphere")
+//    static let somersloop = Part.Static(id: "part-somersloop")
     
     static let specialParts = [
         hardDrive,
         coupon,
-        mercerSphere,
-        somersloop,
+//        mercerSphere,
+//        somersloop,
     ]
 }

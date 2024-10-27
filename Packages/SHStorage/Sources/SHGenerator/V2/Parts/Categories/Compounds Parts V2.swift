13283a5cfa6e0cfb35069a2449b0f67dadc3ff76
Slava Nagornyak <nagornyak.slava@gmail.com>
2024-10-27 15:46:26 +0100

    Fixed compounds are natural resources

--- Packages/SHStorage/Sources/SHGenerator/V2/Parts/Categories/Compounds Parts V2.swift
+++ Packages/SHStorage/Sources/SHGenerator/V2/Parts/Categories/Compounds Parts V2.swift
@@ -2,14 +2,14 @@ import SHModels
 import SHStaticModels
 
 private extension Part.Static {
-    init(id: String, isNaturalResource: Bool = true) {
-        self.init(id: id, category: .compounds, form: .solid, isNaturalResource: isNaturalResource)
+    init(id: String) {
+        self.init(id: id, category: .compounds, form: .solid)
     }
 }
 
 extension V2.Parts {
     static let concrete = Part.Static(id: "part-concrete")
-    static let compactedCoal = Part.Static(id: "part-compacted-coal", isNaturalResource: false)
+    static let compactedCoal = Part.Static(id: "part-compacted-coal")
     static let quartzCrystal = Part.Static(id: "part-quartz-crystal")
     static let silica = Part.Static(id: "part-silica")
     static let aluminumScrap = Part.Static(id: "part-aluminum-scrap")
